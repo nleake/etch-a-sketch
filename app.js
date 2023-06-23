19534d9061014627b7ca01213161a050110de2f4
nleake <6044084+nleake@users.noreply.github.com>
2023-06-23 15:52:31 -0400

    update styles, add buttons

--- app.js
+++ app.js
@@ -1,16 +1,32 @@
 const screen = document.querySelector(".screen");
 const root = document.querySelector(":root");
+const clear = document.querySelector(".clear");
+const bigButton = document.querySelector(".big");
+const smallButton = document.querySelector(".small");
+const mediumButton = document.querySelector(".medium");
+
+clear.addEventListener("click", clearDrawing);
+smallButton.addEventListener("click", () => { makeScreen(16)});
+mediumButton.addEventListener("click", () => { makeScreen(32)});
+bigButton.addEventListener("click", () => { makeScreen(64)});
 
 function draw() {
     return this.classList.add("colored")
 }
 
-function makeGrid (numSquares) {
+function clearDrawing(){
+    let squares = document.querySelectorAll(".square");
+    for (square of squares) {
+        square.classList.remove("colored");
+    }
+}
+
+function makeScreen (numSquares) {
+    screen.innerHTML = "";
   root.style.setProperty("--grid-cols", `${numSquares}`)
   root.style.setProperty("--grid-rows", `${numSquares}`)
   for (i = 0; i < (numSquares * numSquares); i++) {
     let square = document.createElement("div");
-    square.innerText = (i + 1);
     screen.appendChild(square).className = "square";
     square.addEventListener("mouseenter", draw);
   };
@@ -18,7 +34,4 @@ function makeGrid (numSquares) {
 };
 
 
-makeGrid(16);
-
-
-
+makeScreen(16)
